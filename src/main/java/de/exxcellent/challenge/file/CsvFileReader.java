bae702f77fe9bea84fa00f3adf4174814f4feb8a
simon.ruttmann <simon.ruttmann@freenet.de>
2023-01-04 13:23:07 +0100

    feature(CsvFileReader): Added reading of schema and lines

--- src/main/java/de/exxcellent/challenge/file/CsvFileReader.java
+++ src/main/java/de/exxcellent/challenge/file/CsvFileReader.java
@@ -1,14 +1,17 @@
 package de.exxcellent.challenge.file;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Reads the file
  */
 public class CsvFileReader {
+
+    private static final String DELIMITER = ";";
 
     public CsvContent read(String pathToFile) throws IOException {
         CsvContent fileContent = new CsvContent();
@@ -23,10 +26,33 @@ public class CsvFileReader {
 
     private void readSchema(BufferedReader br, CsvContent fileContent) throws IOException {
         String schemaHeader = br.readLine();
-        if(schemaHeader == null) throw new InvalidCsvFormatException("Cannot read csv schema");
+        if(schemaHeader == null) {
+            throw new InvalidCsvFormatException("Cannot read csv schema");
+        }
+        var headerRecord = valuesToList(schemaHeader);
+        fileContent.setSchema(headerRecord);
     }
 
-    private void readLines(BufferedReader br, CsvContent fileContent){
+    private void readLines(BufferedReader br, CsvContent currentFileContent) throws IOException {
 
+        int amountSchemaFields = currentFileContent.getSchema().size();
+        int row = 0;
+
+        String line;
+        while((line = br.readLine()) != null){
+            var splitValues = valuesToList(line);
+            var amountValues = splitValues.size();
+            row++;
+
+            if(amountValues != amountSchemaFields){
+                throw new InvalidCsvFormatException("Not enough values on this line");
+            }
+            currentFileContent.addRow(0, splitValues);
+        }
+    }
+
+    private List<String> valuesToList(String row){
+        var values = row.split(DELIMITER);
+        return Arrays.asList(values);
     }
 }
